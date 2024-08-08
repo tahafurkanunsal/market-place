ae2a1c4c55b3e1f55c3e781c924dac8bff2d2cd0
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2024-08-08 18:02:40 +0300

    Added image dto class

---
+++ src/main/java/com/tfunsal/marketplace/dto/ImageDto.java
@@ -0,0 +1,12 @@
+package com.tfunsal.marketplace.dto;
+
+import lombok.Data;
+
+@Data
+public class ImageDto {
+
+    private Long imageId;
+    private String imageName;
+    private String downloadUrl;
+
+}
