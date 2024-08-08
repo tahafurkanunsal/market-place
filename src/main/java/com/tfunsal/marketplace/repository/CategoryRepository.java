d54554cfecf03fbd3799df93e6503d17066a48a8
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2024-08-08 03:45:45 +0300

    added model repositories

---
+++ src/main/java/com/tfunsal/marketplace/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.tfunsal.marketplace.repository;
+
+import com.tfunsal.marketplace.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category , Long> {
+    Category findByName(String name);
+}
