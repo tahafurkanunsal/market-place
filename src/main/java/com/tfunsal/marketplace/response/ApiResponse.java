0cbafb1d3e9bd4d5410367b79e9cd0a4bc81e7bc
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2024-08-08 18:01:22 +0300

    feat: Add ApiResponse class to standardize API responses

---
+++ src/main/java/com/tfunsal/marketplace/response/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.tfunsal.marketplace.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ApiResponse {
+
+    private String message;
+    private Object data;
+}
