0621acc9a963dcd124cbe16fe868e865bea6fffc
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2024-08-12 23:59:54 +0300

    created cart and cartItem model

---
+++ src/main/java/com/tfunsal/marketplace/repository/CartItemRepository.java
@@ -0,0 +1,10 @@
+package com.tfunsal.marketplace.repository;
+
+import com.tfunsal.marketplace.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    void deleteAllByCartId(Long id);
+}
