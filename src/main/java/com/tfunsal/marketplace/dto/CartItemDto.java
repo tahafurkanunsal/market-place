ab9f117a1418aad4a7a2c70db273e9bc06ebbb2f
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2024-08-21 17:04:40 +0300

    feat: Added CartDto and CartItemDto for cart management

---
+++ src/main/java/com/tfunsal/marketplace/dto/CartItemDto.java
@@ -0,0 +1,14 @@
+package com.tfunsal.marketplace.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class CartItemDto {
+
+    private Long itemId;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private ProductDto product;
+}
